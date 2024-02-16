ce5fed0e578fbcb945c343f347c5a8b1f0918579
performeru <130340605+performeru@users.noreply.github.com>
2024-02-17 02:30:10 +0900

    card setting

---
+++ Matching/Assets/Script/Board.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cardPrefad;
+
+    [SerializeField]
+    private Sprite[] cardSprites;
+
+    private List<int> cardIDList = new List<int>();
+
+    void Start()
+    {
+        GenerateCardID();
+        InitBoard();
+    }
+
+    void GenerateCardID()
+    {
+        for(int i = 0; i < cardSprites.Length; i++)
+        {
+            cardIDList.Add(i);
+            cardIDList.Add(i);
+            cardIDList.Add(i);
+            cardIDList.Add(i);
+        }
+
+    }
+
+    void InitBoard()
+    {
+        float spaceY = 1.9f;
+        float spaceX = 1.3f;
+
+        int rowCount = 6;
+        int colCount = 4;
+
+        int cardIndex = 0; 
+
+        for(int row = 0;  row < rowCount; row++)
+        {
+            for(int col = 0; col < colCount; col++)
+            {
+                float posX = (col - (colCount / 2)) * spaceX + (spaceX / 2);
+                float posY = (row - (int)(rowCount / 2)) * spaceY + (spaceY / 2);
+                Vector3 pos = new Vector3(posX, posY, 0f);
+                GameObject cardObject = Instantiate(cardPrefad, pos, Quaternion.identity);
+                Card card = cardObject.GetComponent<Card>();
+
+                int cardID = cardIDList[cardIndex];
+                card.SetCardID(cardID);
+                card.SetAnimalSprite(cardSprites[cardID]);
+                cardIndex++;
+            }
+        }
+
+    }    
+}
